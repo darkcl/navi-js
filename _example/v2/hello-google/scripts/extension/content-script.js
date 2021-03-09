a4de7ae417b6cec49a2ec5e046581df456b5d2db
Yeung Yiu Hung <446404+darkcl@users.noreply.github.com>
2021-03-09 21:09:55 +0800

    chore(example): use cdn to load library

--- _example/v2/hello-google/scripts/extension/content-script.js
+++ _example/v2/hello-google/scripts/extension/content-script.js
@@ -1,15 +1,23 @@
 console.log("Injected Script");
-window.ipc.setContext({ name: "hello-google", domain: "content" });
 
-console.log("[Content] IPC", window.ipc);
+fetch("https://unpkg.com/navi-chrome-js@1.0.0/dist/navi.min.js")
+  .then((res) => {
+    return res.text()
+  })
+  .then((script) => {
+    eval(script);
+    window.ipc.setContext({ name: "hello-google", domain: "content" });
 
-// Setup Proxy
-window.ipc.setup();
+    console.log("[Content] IPC", window.ipc);
 
-const ipcScript = document.createElement("script");
-ipcScript.src = chrome.extension.getURL("scripts/vendor/navi.min.js");
-document.documentElement.appendChild(ipcScript);
+    // Setup Proxy
+    window.ipc.setup();
 
-const handlerScript = document.createElement("script");
-handlerScript.text = `(${injected.toString()})();`;
-document.documentElement.appendChild(handlerScript);
+    const ipcScript = document.createElement("script");
+    ipcScript.src = "https://unpkg.com/navi-chrome-js@1.0.0/dist/navi.min.js";
+    document.documentElement.appendChild(ipcScript);
+
+    const handlerScript = document.createElement("script");
+    handlerScript.text = `(${injected.toString()})();`;
+    document.documentElement.appendChild(handlerScript);
+  });
