e0542908489e313ae526b488eb7a3c88b9e0828c
Yeung Yiu Hung <446404+darkcl@users.noreply.github.com>
2021-03-10 10:05:22 +0800

    chore(example): update example

--- _example/v2/hello-google/scripts/extension/content-script.js
+++ _example/v2/hello-google/scripts/extension/content-script.js
@@ -1,23 +1,21 @@
 console.log("Injected Script");
 
-fetch("https://unpkg.com/navi-chrome-js@1.0.0/dist/navi.min.js")
-  .then((res) => {
-    return res.text()
-  })
-  .then((script) => {
-    eval(script);
-    window.ipc.setContext({ name: "hello-google", domain: "content" });
+(async () => {
+  const res = await fetch("https://unpkg.com/navi-chrome-js@1.0.0/dist/navi.min.js");
+  const script = await res.text();
+  eval(script);
+  window.ipc.setContext({ name: "hello-google", domain: "content" });
 
-    console.log("[Content] IPC", window.ipc);
+  console.log("[Content] IPC", window.ipc);
 
-    // Setup Proxy
-    window.ipc.setup();
+  // Setup Proxy
+  window.ipc.setup();
 
-    const ipcScript = document.createElement("script");
-    ipcScript.src = "https://unpkg.com/navi-chrome-js@1.0.0/dist/navi.min.js";
-    document.documentElement.appendChild(ipcScript);
+  const ipcScript = document.createElement("script");
+  ipcScript.src = "https://unpkg.com/navi-chrome-js@1.0.0/dist/navi.min.js";
+  document.documentElement.appendChild(ipcScript);
 
-    const handlerScript = document.createElement("script");
-    handlerScript.text = `(${injected.toString()})();`;
-    document.documentElement.appendChild(handlerScript);
-  });
+  const handlerScript = document.createElement("script");
+  handlerScript.text = `(${injected.toString()})();`;
+  document.documentElement.appendChild(handlerScript);
+})();
